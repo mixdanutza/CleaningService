89e858b62e71cbe469e7bbe6b51857f224ee38a1
Daniela Bizgu <bizgudaniela1@gmail.com>
2017-09-26 18:37:13 -0700

    added services section

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-$('.carousel').carousel();
+$("#home").click(function() {
+    console.log("In sript load")
+    $('html,body').animate({
+        scrollTop: $(".header").offset().top},
+        'slow');
+});
+
+$("#contact").click(function() {
+    console.log("In sript load")
+    $('html,body').animate({
+        scrollTop: $(".contact-container").offset().top},
+        'slow');
+});
+
+$("#services").click(function() {
+    console.log("In sript load")
+    $('html,body').animate({
+        scrollTop: $("#wrap").offset().top},
+        'slow');
+});
+
+
+// $("#about").click(function() {
+//     console.log("In sript load")
+//     $('html,body').animate({
+//         scrollTop: $(".about").offset().top},
+//         'slow');
+// });
+
+
